3b566b3ba8a2dedb2dbba6b589d175286297e6e3
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-30 00:38:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "webservers" {
 	count = length(var.subnets_cidr)
 	ami = var.webservers_ami
 	instance_type = var.instance_type
-	security_groups = ["{aws_security_group.webservers.id}"]
+	security_groups = ["{aws_security_group.webservers.name}"]
 	subnet_id = element(aws_subnet.public.*.id,count.index)
 	user_data = file("install_httpd.sh")
 
