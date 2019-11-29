79018372eb126b72fef1692dd2230210a5987f4b
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-29 23:41:33 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -2,8 +2,8 @@
 resource "aws_elb" "terra-elb" {
   name               = "terra-elb"
   #availability_zones = ["${var.azs}"]
-  subnets = ["${aws_subnet.public.*.id}"]
-  security_groups = ["${aws_security_group.webservers.id}"]
+  subnets = aws_subnet.public.*.id
+  security_groups = aws_security_group.webservers.id
 
   listener {
     instance_port     = 80
@@ -20,7 +20,7 @@ resource "aws_elb" "terra-elb" {
     interval            = 30
   }
 
-  instances                   = ["${aws_instance.webservers.*.id}"]
+  instances                   = aws_instance.webservers.*.id
   cross_zone_load_balancing   = true
   idle_timeout                = 100
   connection_draining         = true
@@ -32,5 +32,5 @@ resource "aws_elb" "terra-elb" {
 }
 
 output "elb-dns-name" {
-  value = "${aws_elb.terra-elb.dns_name}"
+  value = aws_elb.terra-elb.dns_name
 }
