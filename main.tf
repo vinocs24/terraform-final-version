4ad1eee3e3e3989b7d721a9638c0379ce5459f80
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-30 00:40:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "webservers" {
 	count = length(var.subnets_cidr)
 	ami = var.webservers_ami
 	instance_type = var.instance_type
-	security_groups = ["{aws_security_group.webservers.name}"]
+	security_groups = ["{aws_security_group.webservers.id}"]
 	subnet_id = element(aws_subnet.public.*.id,count.index)
 	user_data = file("install_httpd.sh")
 
@@ -29,7 +29,7 @@ resource "aws_instance" "webservers" {
 resource "aws_security_group" "webservers" {
   name        = "allow_http"
   description = "Allow http inbound traffic"
-  vpc_id      = aws_vpc.terra_vpc.id
+ # vpc_id      = aws_vpc.terra_vpc.id
 
   ingress {
     from_port   = 80
