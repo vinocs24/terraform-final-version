2078f2a21745a48a590d31f15fc7c891e29f5a2b
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-29 23:43:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ provider "aws" {
 
 # Create EC2 instance
 resource "aws_instance" "webservers" {
-	count = "${length(var.subnets_cidr)}" 
-	ami = "${var.webservers_ami}"
-	instance_type = "${var.instance_type}"
-	security_groups = ["${aws_security_group.webservers.id}"]
-	subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
-	user_data = "${file("install_httpd.sh")}"
+	count = length(var.subnets_cidr)
+	ami = var.webservers_ami
+	instance_type = var.instance_type
+	security_groups = aws_security_group.webservers.id
+	subnet_id = element(aws_subnet.public.*.id,count.index)
+	user_data = file("install_httpd.sh")
 
-	tags {
-	  Name = "Server-${count.index}"
+	tags = {
+	  Name = "Server-count.index"
 	}
 }
