5d44feb78c1d1aa296f366481d48c02861fd14e2
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-29 23:38:45 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,39 +1,39 @@
 # VPC
 resource "aws_vpc" "terra_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  tags {
+  cidr_block       = var.vpc_cidr
+  tags = {
     Name = "TerraVPC"
   }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "terra_igw" {
-  vpc_id = "${aws_vpc.terra_vpc.id}"
-  tags {
+  vpc_id = aws_vpc.terra_vpc.id
+  tags = {
     Name = "main"
   }
 }
 
 # Subnets : public
 resource "aws_subnet" "public" {
-  count = "${length(var.subnets_cidr)}"
-  vpc_id = "${aws_vpc.terra_vpc.id}"
-  cidr_block = "${element(var.subnets_cidr,count.index)}"
-  availability_zone = "${element(var.azs,count.index)}"
+  count = length(var.subnets_cidr)
+  vpc_id = aws_vpc.terra_vpc.id
+  cidr_block = element(var.subnets_cidr,count.index)
+  availability_zone = element(var.azs,count.index)
   map_public_ip_on_launch = true
-  tags {
-    Name = "Subnet-${count.index+1}"
+  tags = {
+    Name = "Subnet-count.index+1
   }
 }
 
 # Route table: attach Internet Gateway 
 resource "aws_route_table" "public_rt" {
-  vpc_id = "${aws_vpc.terra_vpc.id}"
+  vpc_id = aws_vpc.terra_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.terra_igw.id}"
+    gateway_id = aws_internet_gateway.terra_igw.id
   }
-  tags {
+  tags = {
     Name = "publicRouteTable"
   }
 }
