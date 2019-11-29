504c166a206addac91093d04a67e9bea53caf26b
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-29 23:49:02 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -40,7 +40,7 @@ resource "aws_route_table" "public_rt" {
 
 # Route table association with public subnets
 resource "aws_route_table_association" "a" {
-  count = "${length(var.subnets_cidr)}"
-  subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
-  route_table_id = "${aws_route_table.public_rt.id}"
+  count = length(var.subnets_cidr)
+  subnet_id      = element(aws_subnet.public.*.id,count.index)
+  route_table_id = aws_route_table.public_rt.id
 }
