cd1c02749f0a8f461a21a14d202e1124aa804c8b
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-30 00:24:35 +0530

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "webservers" {
+resource "aws_security_group" "webservers1" {
   name        = "allow_http"
   description = "Allow http inbound traffic"
   vpc_id      = aws_vpc.terra_vpc.id
